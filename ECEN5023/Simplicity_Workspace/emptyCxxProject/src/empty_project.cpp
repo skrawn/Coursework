5eccf771f77237bee565cbba1e83853f0de538d4
Sean Donohue <Sean@skrawn.(none)>
2016-01-24 20:30:31 -0700

    Added the SWO function and some other things that may be useful to other projects

--- ECEN5023/Simplicity_Workspace/emptyCxxProject/src/empty_project.cpp
+++ ECEN5023/Simplicity_Workspace/emptyCxxProject/src/empty_project.cpp
@@ -18,18 +18,79 @@
 #include "em_chip.h"
 #include "header.h"
 
+#define MIN_POWER_MODE		1
+
+#if !MIN_POWER_MODE
+Serial pc(USBTX, USBRX);
+#endif
+
+void BSP_TraceSwoSetup(void);
+
+/**************************************************************************//**
+ * @brief Configure trace output for energyAware Profiler
+ * @note  Enabling trace will add 80uA current for the EFM32_Gxxx_STK.
+ *        DK's needs to be initialized with SPI-mode:
+ * @verbatim BSP_Init(BSP_INIT_DK_SPI); @endverbatim
+ *****************************************************************************/
+void BSP_TraceSwoSetup(void)
+{
+	/* Enable GPIO clock */
+	CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+
+	/* Enable Serial wire output pin */
+	GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
+
+	/* Set correct location */
+	/* This location is valid for GG, LG and WG! */
+	GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
+
+	/* Enable output on correct pin. */
+	/* This pin is valid for GG, LG and WG! */
+	GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
+	GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
+
+	/* Enable debug clock AUXHFRCO */
+	CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
+
+	/* Wait until clock is ready */
+	while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY)) ;
+
+	/* Enable trace in core debug */
+	CoreDebug->DHCSR |= CoreDebug_DHCSR_C_DEBUGEN_Msk;
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+
+	/* Enable PC and IRQ sampling output */
+	DWT->CTRL = 0x400113FF;
+
+	/* Set TPIU prescaler to 16. */
+	TPI->ACPR = 15;
+
+	/* Set protocol to NRZ */
+	TPI->SPPR = 2;
+
+	/* Disable continuous formatting */
+	TPI->FFCR = 0x100;
+
+	/* Unlock ITM and output data */
+	ITM->LAR = 0xC5ACCE55;
+	ITM->TCR = 0x10009;
+
+	/* ITM Channel 0 is used for UART output */
+	ITM->TER |= (1UL << 0);
+}
+
 /**************************************************************************//**
  * @brief  Main function
  *****************************************************************************/
 int main(void)
 {
-  /* Chip errata */
-#if THIS_STUFF
-  CHIP_Init();
+	/* Chip errata */
+	CHIP_Init();
+#if !MIN_POWER_MODE
+	BSP_TraceSwoSetup();
 #endif
 
-
-  /* Infinite loop */
-  while (1) {
-  }
+	/* Infinite loop */
+	while (1) {
+	}
 }
