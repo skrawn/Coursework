88511106a5717a35d468c54cd0895149f1fa08fc
Sean Donohue <sdonohue331@gmail.com>
2016-07-23 18:26:23 -0600

    Setting up timelapse project

---
+++ ECEN5623/workspace/Timelapse/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 23, 2016
+ *      Author: skrawn
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+	printf("Hello world!\r\n");
+	return 0;
+}
