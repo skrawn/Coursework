e89bbd5587202c7abb519e2bcb44bd5295fdd7c3
Sean Donohue <sdonohue331@gmail.com>
2016-07-31 19:25:35 -0600

    Added Enable_All_Core.sh script.

---
+++ ECEN5623/workspace/Timelapse/Enable_All_Cores.sh
@@ -0,0 +1,8 @@
+
+sudo echo 0 > /sys/deivces/system/cpu/cpuquiet/tegra_cpuquiet/enable
+sudo echo 1 > /sys/devices/system/cpu/cpu0/online
+sudo echo 1 > /sys/devices/system/cpu/cpu1/online
+sudo echo 1 > /sys/devices/system/cpu/cpu2/online
+sudo echo 1 > /sys/devices/system/cpu/cpu3/online
+echo "All cores enabled!"
+exit
