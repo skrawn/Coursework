c69c625631c2fd7d89ca3cee03c49a66759b0805
Sean Donohue <sdonohue331@gmail.com>
2016-10-11 21:58:28 -0600

    Fixed the FRDM timer code

--- ECEN5013/Project1/src/profiler.c
+++ ECEN5013/Project1/src/profiler.c
@@ -10,6 +10,7 @@
 #define PROFILER_CH		0
 #define NSEC_PER_USEC 	1000
 #define TIMER_FREQUENCY	1000000
+#define TIMER_MOD_VAL	0x7FFF
 
 void profiler_init(void)
 {
@@ -18,11 +19,13 @@ void profiler_init(void)
 	// Ungate the clock for TPM1
 	SIM->SCGC6 |= SIM_SCGC6_TPM1(1);
 	
-	TPM1->SC = TPM_SC_CPWMS(1) |	// Upcounting
+	TPM1->SC = TPM_SC_CPWMS(0) |	// Upcounting
 			TPM_SC_PS(5);			// Prescaler of 32, 48M/32 = 1.5MHz
+			//TPM_SC_PS(0);			// Prescaler of 1, 48M/1 = 48MHz
 
 	// Set the period
-	TPM1->MOD = (1500000 / TIMER_FREQUENCY);
+	//TPM1->MOD = (48000000 / TIMER_FREQUENCY);
+	TPM1->MOD = 0x7FFF;
 
 	// Configure as simply a free-running counter
 	TPM1->CONTROLS[PROFILER_CH].CnSC = 0x00 |
@@ -39,7 +42,9 @@ uint32_t profiler_get_time(void)
 // FRDM uses a hardware timer for profiling. BBB and host linux system
 // can use the time functions
 #ifdef FRDM
-	return (TPM1->CNT & 0xFFFF);
+	// Timer clock is running at about 1.5MHz, so adjust the time for that.
+	uint32_t count = (TPM1->CNT & 0xFFFF) * 2 / 3;
+	return count;
 #else
 	struct timespec time;
 
@@ -54,10 +59,10 @@ uint32_t profiler_get_time(void)
 uint32_t profiler_get_time_diff(uint32_t start, uint32_t end)
 {
 	// Handle wrap-around
-	if (end > start) {
+	if (end < start) {
 		// FRDM uses a 16-bit timer
 #ifdef FRDM
-		return (UINT16_MAX - start) + end;
+		return (TIMER_MOD_VAL - start) + end;
 #else
 		return (UINT32_MAX - start) + end;
 #endif
