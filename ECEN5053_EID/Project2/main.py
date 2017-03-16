94b1c7439a7b04a1300d4fa4ac1598286ae95aed
Sean Donohue <sdonohue331@gmail.com>
2017-03-15 22:26:48 -0600

    Added periodic updates, implemented quit and temperature slider

--- ECEN5053_EID/Project2/main.py
+++ ECEN5053_EID/Project2/main.py
@@ -6,13 +6,11 @@ from PyQt5 import QtCore
 
 import time
 import threading
-from threading import Thread, Lock
+from threading import Thread
 
 import mainwindow_auto
 
 import Adafruit_DHT
-
-mutex = Lock()
 
 
 class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
@@ -28,19 +26,44 @@ class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
                 sleep_time = 5
             time.sleep(sleep_time)
 
+    def celsiusToFahrenheit(self, tempC):
+        return (tempC * 9 / 5 + 32)
+
     def updateSensor(self):
-        mutex.acquire()
+        global useTempF
         _translate = QtCore.QCoreApplication.translate
+        self.txtStatus.setText(_translate("MainWindow", "Updating"))
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+        if (useTempF):
+            self.txtTemp.setText(_translate("MainWindow", '{0:0.1f} F'.format(
+                self.celsiusToFahrenheit(temperature))))
+        else:
+            self.txtTemp.setText(_translate("MainWindow", '{0:0.1f} C'.format(
+                temperature)))
+
+        self.txtHumidity.setText(_translate("MainWindow", '{0:0.1f} %'.format(humidity)))
+        self.txtStatus.setText(_translate("MainWindow", "Connected"))
 
     # Button click handlers
+    def pressedQuit(self):
+        quit_app = True
+        exit(1)
+
+    def tempSliderChanged(self):
+        global useTempF
+        value = self.sldrTemp.value()
+        if (value == 0):
+            useTempF = False
+        else:
+            useTempF = True
 
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
 
         # Button hooks
-        #self.sldrTemp.
+        self.sldrTemp.valueChanged.connect(lambda: self.tempSliderChanged())
+        self.btnQuit.clicked.connect(lambda: self.pressedQuit())
 
         # Periodic thread for automatic updates
         timerThread = threading.Thread(target=self.updateSensor_5s)
@@ -50,6 +73,7 @@ class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
 
 quit_app = False
 updating = False
+useTempF = False
 
 
 def main():
